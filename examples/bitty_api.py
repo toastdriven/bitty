c3ac7077f69970604a4e3ffe614564255b23c3c3
Daniel Lindsley <daniel@toastdriven.com>
2009-12-14 09:16:14 -0600

    Fixed up the example file a little. Still need to support a couple things from it.

--- examples/bitty_api.py
+++ examples/bitty_api.py
@@ -1,8 +1,10 @@
 from bitty import Bitty
 import datetime
+import os
 
 # Connect.
 bit = Bitty('sqlite://test.db')
+bit.raw("""CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), age INTEGER NULL, created DATETIME NULL);""")
 
 # Insert.
 bit.add('people', id=1, name='Daniel', age=27, created=datetime.datetime.now())
@@ -11,13 +13,13 @@ bit.add('people', id=3, name='Sean', age=3, created=datetime.datetime.now())
 bit.add('people', id=4, name='Chester', age=1, created=datetime.datetime.now())
 
 # Update?
-bit.update('people', id=1, name='Toast Driven')
+bit.update('people', 1, name='Toast Driven')
 
 # Get.
 toast = bit.get('people', id=1)
 
 # Select.
-if len(bit.find('people', id=1))):
+if len(bit.find('people', id=1)):
     for result in bit.find('people', id=1):
         print result['name']
 
@@ -27,8 +29,11 @@ for result in bit.find('people', name=['Moriah', 'Sean']):
 for result in bit.find('people', age__lte=20):
     print result['name']
 
-for result in bit.find('people', name=['Moriah', 'Sean'], age__lte=20)
+for result in bit.find('people', name=['Moriah', 'Sean'], age__lte=20):
     print result['name']
 
 # Delete?
-bit.delete('people', id=1)
+bit.delete('people', 1)
+
+# Nuke the DB so this script can be run standalone.
+os.remove('test.db')
